25185fecce36c0c875816c892cd347785208a306
offiong-funmilola <fawolefunmilola2@gmail.com>
2023-03-06 23:30:15 +0100

    logo removed

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
-import logo from './Assets/logo.png'
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 function Nav() {
